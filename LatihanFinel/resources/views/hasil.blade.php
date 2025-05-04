1cb431d947db9d3ebe8c59114633bf4a6a33c059
Hessigawa <finelgahessigawa_2428250012@mhs.mdp.ac.id>
2025-05-04 10:50:39 +0700

    LatihanFinel

---
+++ LatihanFinel/resources/views/hasil.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h5>Hasil Penjumlahan</h5>
+    <?php echo $angka1. "+";?> <?php echo $angka2. "+";?> <?php echo $angka3;?>
+    <?php echo 'hasil : '. ($angka1 + $angka2 + $angka3) ?>
+</div>
